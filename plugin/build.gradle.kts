a6990c62093d8671334c62df0365c5b34a3c02f1
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2026-07-12 22:32:26 -0400

    build(plugin): configure plugin to release to modrinth

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar.Companion.shadowJar
+import dev.hboyd.git_simple_semver.GitSimpleSemverExtension
 
 plugins {
     id("simplefreeze.common-conventions")
@@ -6,9 +7,10 @@ plugins {
     alias(libs.plugins.paperLoaderGen)
     //alias(libs.plugins.paperweight) // Should only be used for development
     alias(libs.plugins.gradleShadow)
+    alias(libs.plugins.minotaur)
 }
 
-runPaper.folia.registerTask {  }
+runPaper.folia.registerTask { }
 
 dependencies {
     //paperweight.paperDevBundle(libs.versions.paperAPI) // Should only be used for development
@@ -54,4 +56,26 @@ tasks {
     jar {
         enabled = false // Only shadowed jar
     }
+}
+
+
+modrinth {
+    token = providers.gradleProperty("hboydModrinthToken")
+        .orElse(providers.environmentVariable("HBOYD_MODRINTH_TOKEN"))
+        .orNull
+    projectId = "simple_freeze"
+    versionNumber = rootProject.extensions.getByType(GitSimpleSemverExtension::class.java).version.buildVersionString(
+        includePreReleaseLabel = true,
+        includeBuildMetadataLabel = version.toString().endsWith("-SNAPSHOT")
+    )
+    versionType = if (versionNumber.toString().endsWith("-SNAPSHOT")) "beta" else "alpha"
+    changelog = providers.environmentVariable("CHANGELOG")
+    uploadFile = tasks.shadowJar.get().archiveFile.get()
+    loaders = listOf("paper", "purpur", "folia")
+    gameVersions = listOf("1.21.8", "1.21.9", "1.21.10", "1.21.11", "26.1")
+    dependencies {
+        required.project("packetevents")
+        // TODO: Add chasm companion plugin when it is published to modrinth
+    }
+    syncBodyFrom = rootProject.file("README.md").readText()
 }
