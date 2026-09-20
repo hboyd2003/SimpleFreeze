d97f511dae99039b16e0dd8a25f8a5763ee9d302
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2026-09-20 01:41:26 -0400

    chore(deps): update Adventure to 5.2.0

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * SimpleFreeze is a plugin for Minecraft Paper that enables quick and efficient Player freezing for Admins and other plugins.
  */
 module SimpleFreeze.simplefreeze.api.main {
-    requires transitive net.kyori.adventure;
+    requires transitive net.kyori.adventure.api;
     requires transitive net.kyori.adventure.key;
     requires transitive org.jetbrains.annotations;
     requires transitive org.jspecify;
